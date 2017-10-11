f5f302b334cb9f7042e5ef2fb79bf37e30bbc8de
Xochitl Fierros <xochitlfm@gmail.com>
2017-10-11 20:14:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function destructivelyAppendKitten(name) {
   return kittens
 
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+kittens.pop();
+
+}
